d292e1620d003469d9aff0f859c6f1033a2b5791
Ayrton Gomes <com.ayrton@gmail.com>
2017-11-13 07:49:08 -0100

    new api class

---
+++ src/main/java/com/dk/trender/resources/ZChannelApi.java
@@ -0,0 +1,112 @@
+package com.dk.trender.resources;
+
+import java.util.List;
+
+import javax.annotation.security.PermitAll;
+import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.dk.trender.core.ZChannel;
+import com.dk.trender.core.ZCollection;
+import com.dk.trender.service.ZChannelService;
+import com.dk.trender.service.ZCollectionService;
+
+import io.dropwizard.hibernate.UnitOfWork;
+
+/**
+ * 
+ * @author ayrton
+ * @date 2017-04-02 18:36:53
+ */
+@Path("/api/v1/channel")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class ZChannelApi {
+	private static final Logger log = LoggerFactory.getLogger(ZChannelApi.class);
+	private final ZChannelService $channel;
+	public ZChannelApi(ZChannelService channel) {
+		this.$channel = channel;
+	}
+
+	@GET
+	@Path("/")
+	@UnitOfWork
+	public List<ZChannel> all() {
+		return $channel.all();
+	}
+
+	@GET
+	@Path("/{id}")	
+	@UnitOfWork
+	public ZChannel get(@PathParam("id") long id) {
+		return $channel.byId(id);
+	}
+
+	@GET
+	@Path("/find")	
+	@UnitOfWork
+	public List<ZChannel> find(@QueryParam("audience") 
+						       @DefaultValue(ZChannel.PUBLIC)
+							   String audience) {
+		return $channel.find(audience);
+	}
+
+	/**
+	 * 
+	 * TODO: fix duplicate creation of channels
+	 * @return
+	 */
+	@GET
+	@Path("/find_by")
+	@UnitOfWork
+	public ZChannel findBy(@QueryParam("name") @NotEmpty String name,
+						   @QueryParam("q") @NotEmpty String q) {
+		return $channel.findByName(name, q);
+	}
+
+	@GET
+	@Path("/{id}/collections")	
+	@UnitOfWork
+	public List<ZCollection> getCols(@PathParam("id") long id) {
+		return $channel.collections(id);
+	}
+
+	@POST
+	@Path("/new")
+	@UnitOfWork
+	@PermitAll
+	public ZChannel create(@Valid ZChannel req) {		
+		return $channel.create(req);
+	}
+	
+	@POST
+	@Path("/{id}")	
+	@UnitOfWork
+	public ZChannel update(@PathParam("id") long id,
+						   @Valid ZChannel chan) {
+		return $channel.update(chan);
+	}
+
+	@POST
+	@Path("/{id}/delete")	
+	@UnitOfWork
+	@PermitAll
+	public void delete(@PathParam("id") long id) {
+		$channel.delete(id);
+	}
+}
+
